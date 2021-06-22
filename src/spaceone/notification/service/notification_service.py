ffa6c3928f16048ec206e896566a99fefe93d87a
Jihyung Song <bluese@megazone.com>
2021-06-22 16:49:50 +0900

    update message format

--- src/spaceone/notification/service/notification_service.py
+++ src/spaceone/notification/service/notification_service.py
@@ -1,6 +1,7 @@
 import logging
 
 from spaceone.core.service import *
+from spaceone.notification.conf.slack_conf import SLACK_CONF
 from spaceone.notification.manager.notification_manager import NotificationManager
 
 _LOGGER = logging.getLogger(__name__)
@@ -33,10 +34,18 @@ class NotificationService(BaseService):
         channel = channel_data.get('channel')
 
         noti_mgr: NotificationManager = self.locator.get_manager('NotificationManager')
-        message_block = self.make_slack_message_block(message)
+        # message_block = self.make_slack_message_block(message, notification_type)
+        message_block = []
+        message_attachment = self.make_slack_message_attachment(message, notification_type)
+
+        if message_attachment:
+            kwargs = {
+                'attachments': message_attachment
+            }
+
         noti_mgr.dispatch(slack_token, channel, message_block, **kwargs)
 
-    def make_slack_message_block(self, message):
+    def make_slack_message_block(self, message, notification_type):
         '''
         message (dict): {
             'title': 'str',
@@ -57,14 +66,7 @@ class NotificationService(BaseService):
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": f"{message.get('title', '')}"
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "plain_text",
-                    "text": f"{message.get('description', '')}"
+                    "text": f"[{notification_type}] {message.get('title', '')}"
                 }
             }
         ]
@@ -73,7 +75,7 @@ class NotificationService(BaseService):
         if contexts:
             blocks.extend(contexts)
 
-        buttons = self.make_callback_button(message.get('callbacks', []))
+        buttons = self.make_callback_buttons(message.get('callbacks', []))
         if buttons:
             blocks.append({
                 "type": "actions",
@@ -82,8 +84,64 @@ class NotificationService(BaseService):
 
         return blocks
 
+    def make_slack_message_attachment(self, message, notification_type):
+        '''
+        message (dict): {
+            'title': 'str',
+            'description': bool,
+            'tags': dict,
+            'callbacks': [
+              {
+                'label': 'str',
+                'url': 'str',
+                'options': 'dict'
+              }
+            ]
+        }
+        '''
+        attachments = []
+
+        attachment = {
+            "pretext": f"[{notification_type}] {message.get('title', '')}",
+            "color": self.get_message_attachment_color(notification_type),
+            "text": f"{message.get('description', '')}",
+            'mrkdwn_in': ['text'],
+            'footer': 'From. SpaceONE',
+            'footer_icon': 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/wanny.png'
+        }
+
+        if 'tags' in message:
+            attachment['fields'] = self.make_fields_from_tags(message['tags'])
+
+        if 'callbacks' in message:
+            attachment['actions'] = self.make_callback_buttons_in_attachment(message['callbacks'])
+
+        attachments.append(attachment)
+
+        return attachments
+
     @staticmethod
-    def make_callback_button(callbacks):
+    def make_callback_buttons_in_attachment(callbacks):
+        actions = []
+
+        for _callback in callbacks:
+            actions.append({
+                'name': _callback.get('label', ''),
+                'text': _callback.get('label', ''),
+                'type': 'button',
+                'style': 'danger',
+                "url": f"{_callback.get('url', '')}",
+            })
+
+        return actions
+
+    @staticmethod
+    def get_message_attachment_color(notification_type):
+        color_map = SLACK_CONF.get('attachment_color_map', {})
+        return color_map.get(notification_type, color_map.get('default', '#858895'))
+
+    @staticmethod
+    def make_callback_buttons(callbacks):
         buttons = []
 
         for callback in callbacks:
@@ -120,3 +178,16 @@ class NotificationService(BaseService):
             })
 
         return contexts
+
+    @staticmethod
+    def make_fields_from_tags(tags):
+        fields = []
+
+        for key, value in tags.items():
+            fields.append({
+                'title': f'{key}',
+                'value': f'{value}',
+                'short': True
+            })
+
+        return fields
